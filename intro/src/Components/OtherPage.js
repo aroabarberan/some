9d710c26ad040fc00aa1d93f12ebcf52779023b4
Aroa Barberan Poblete <aroabarberan@gmail.com>
2018-07-09 23:57:19 +0200

    remove commets

--- intro/src/Components/OtherPage.js
+++ intro/src/Components/OtherPage.js
@@ -2,23 +2,18 @@ import React, { Component } from 'react';
 import { Head, Content } from './SuperWeb';
 
 export default class OtherPage extends Component {
-    constructor() {
-        super()
-        this.state = {
-            components: <div></div>
-        }
-        this.click = this.click.bind()
-    }
-    click() {
-        // this.setState({ components: <div><Head click={this.click} /><Content /></div> })
-        this.props.click()
-    }
-    render() {
-        return (
-            <div>
-                {this.state.components}
-                <h1>This is other page!!!!</h1>
-            </div>
-        )
-    }
+  constructor() {
+    super()
+    this.click = this.click.bind()
+  }
+  click() {
+    this.props.click()
+  }
+  render() {
+    return (
+      <div>
+        <h1>This is other page!!!!</h1>
+      </div>
+    )
+  }
 }
