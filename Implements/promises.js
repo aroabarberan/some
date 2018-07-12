cce70c6b1a8d0682bdf4a2134384a6b27eb4140c
Aroa Barberan Poblete <aroabarberan@gmail.com>
2018-07-12 23:37:56 +0200

    fix function and add reject

--- Implements/promises.js
+++ Implements/promises.js
@@ -1,11 +1,13 @@
 const fs = require('fs')
 
 function readFilePromise(file, encoding = 'utf-8') {
-  return new Promise(resolve => {
-    fs.readFile(file, encoding,(err, data) => {
-      !err ? resolve(data) : console.log(err)
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, encoding, (err, data) => {
+      !err ? resolve(data) : reject(err)
     })
   })
 }
 
-readFilePromise('text.txt').then(console.log)
+readFilePromise('tesxt.txt')
+  .then(console.log)
+  .catch(console.log)
