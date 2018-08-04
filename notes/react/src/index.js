5f4f8a4b4fe9ad39549bbe7c4c087a237767a48e
Aroa Barberan Poblete <aroabarberan@gmail.com>
2018-08-05 01:11:51 +0200

    get data to backend

--- notes/react/src/index.js
+++ notes/react/src/index.js
@@ -1,1 +1,5 @@
-import ShowNotes from './components/ShowNotes';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './App'
+
+ReactDOM.render(<App />, document.getElementById('root'))
