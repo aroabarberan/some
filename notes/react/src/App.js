a1d786e6bd60448c0bcc27c6b4b057fe4bb42714
Aroa Barberan Poblete <aroabarberan@gmail.com>
2018-08-06 00:38:46 +0200

    show notes to page

--- notes/react/src/App.js
+++ notes/react/src/App.js
@@ -8,30 +8,30 @@ class App extends React.Component {
     this.state = {
       data: []
     };
+  }
+
+  componentWillMount() {
     fetch('http://127.0.0.1:8000/notes')
       .then(res => res.json())
       .then(json => json.results)
       .then(data => this.setState({ data }))
+      .catch(console.log)
   }
-
-  // componentWillMount() {
-  //   fetch('http://127.0.0.1:8000/notes')
-  //     .then(res => res.json())
-  //     .then(json => json.results)
-  //     .then(data => this.setState({ data }))
-  //     .catch(console.log)
-  // }
 
   render() {
     console.log(this.state.data.length)
     if (this.state.data.length > 0) {
       return (
         <div>
+          
+          <h1>Notas</h1>
           {this.state.data.map(n => {
-            <div key={n.id}>
-              <p>{n.title}</p>
-              <p>{n.content}</p>
-            </div>
+            return (
+              <div key={n.id}>
+                <p>Titulo: {n.title}</p>
+                <p>Content: {n.content}</p>
+              </div>
+            )
           })}
         </div>
       )
